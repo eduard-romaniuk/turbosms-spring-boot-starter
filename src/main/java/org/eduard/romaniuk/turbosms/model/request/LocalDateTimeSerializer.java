1ed8ed602c0e2c669ae89aa737fceeddc96881a4
Eduard Romanyuk <eduard.romanyuk1990@gmail.com>
2020-11-25 22:44:07 +0200

    feat: add all available sms properties

---
+++ src/main/java/org/eduard/romaniuk/turbosms/model/request/LocalDateTimeSerializer.java
@@ -0,0 +1,20 @@
+package org.eduard.romaniuk.turbosms.model.request;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeSerializer extends StdSerializer<LocalDateTime> {
+    protected LocalDateTimeSerializer() {
+        super(LocalDateTime.class);
+    }
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator json, SerializerProvider provider) throws IOException {
+        json.writeString(value.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm")));
+    }
+}
--- src/main/java/org/eduard/romaniuk/turbosms/model/request/TurboSmsMessage.java
+++ src/main/java/org/eduard/romaniuk/turbosms/model/request/TurboSmsMessage.java
@@ -1,7 +1,10 @@
 package org.eduard.romaniuk.turbosms.model.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public interface TurboSmsMessage {
     List<String> getRecipients();
 
